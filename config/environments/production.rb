bc245326e399de9c3dc81fb8bbc4178695b689b1
X X <mwfcomp@gmail.com>
2012-11-18 16:51:21 +1100

    adding production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,20 @@ BasicSite::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Don't care if the mailer can't send                                                  
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'boogifdev@gmail.com',
+    :password             => 'aBLrsT37',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
