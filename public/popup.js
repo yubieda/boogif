369c96ef6e71e95579b3b74db4c445723898094f
X X <mwfcomp@gmail.com>
2012-11-26 19:14:53 +1100

    adding popup to public for real

---
+++ public/popup.js
@@ -0,0 +1,42 @@
+$(function(){
+    wrapPopup('.popup-frame','.popup')
+});
+
+function dynamicCenter(item) {
+    var windowW = $(window).width();// document.documentElement.clientWidth;
+    var windowH = $(window).height(); //document.documentElement.clientHeight;
+    var popupH = $(item).height();
+    var popupW = $(item).width();
+    $(item).css('position', 'absolute');
+    $(item).css('top', windowH/2-popupH/2);
+    $(item).css('left', windowW/2-popupW/2);
+    return item;
+}
+
+function displayPopupAndFrame(item) {
+    $('.popup-background').fadeIn('slow');
+    var frame = $(item).parents('.popup-frame');
+    dynamicCenter(frame);
+    frame.fadeIn('slow');
+}
+
+function displayPopup(item) {
+    dynamicCenter(item);
+    item.fadeIn('slow');    
+}
+
+
+
+//manipulate DOM to wrap popup in frame and background
+function wrapPopup(frame, popup) {
+    $(popup).wrap($(frame));
+}
+
+function exitPopup(selector) {
+    $('.popup-background').fadeOut('slow');
+    $(selector).fadeOut('slow');
+//    $('.popup-frame').fadeOut('slow');
+
+    //$(item).parents('.popup-frame').fadeToggle('slow');
+}
+
