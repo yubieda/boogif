f8d927507c24318acab3c7f26756282ecfab0b77
Alex Mishyn <mishyn@gmail.com>
2013-02-18 16:00:23 +0200

    fixed popup positioning

--- app/assets/javascripts/included/items.js
+++ app/assets/javascripts/included/items.js
@@ -1,39 +1,39 @@
 $(function(){
-    $('.item-image-and-title').click(function() {
-	displayPopupAndFrame($(this).siblings().find('.item-dialog-pop'));
-    });
-    
-    $('.popup-exit').click(function() {
-        exitPopup('.popup-frame');
-    });
+  $('.item-image-and-title').click(function() {
+    displayPopupAndFrame($(this).siblings().find('.item-dialog-pop'));
+  });
 
-    $('.purchase').change(function() {
-	if(this.checked) {
-	    alert("We'll hide this item from other friends, to avoid repeat gifts!");
-	    $.ajax(add_query_string_param("/purchase_item","id",this.id));
-	}
-	else {
-	    alert("We'll start showing this to other friends, so someone else can buy it!");
-	    $.ajax(add_query_string_param("/unpurchase_item","id",this.id));
-	}
-		
-    });
+  $('.popup-exit').click(function() {
+    exitPopup('.popup-frame');
+  });
 
-    $('.add-gift-exit').click(function() {
-	parent.location = parent.location;
-	//parent.exitPopup('.add-gift');
+  $('.purchase').change(function() {
+    if(this.checked) {
+      alert("We'll hide this item from other friends, to avoid repeat gifts!");
+      $.ajax(add_query_string_param("/purchase_item","id",this.id));
+    }
+    else {
+      alert("We'll start showing this to other friends, so someone else can buy it!");
+      $.ajax(add_query_string_param("/unpurchase_item","id",this.id));
+    }
 
-	
-	//alert($('.add-gift').length);
-	//$('.add-gift-div').css('color','red');
-	//$('h1').css('color','red');
-	//$('.add-gift').fadeOut();
-//	alert('hello');
-//	window.close();
-	
-	//window.close();
-//	$('add-gift').fadeOut();
-    });
-    
+  });
+
+  $('.add-gift-exit').click(function() {
+    parent.location = parent.location;
+    //parent.exitPopup('.add-gift');
+
+
+    //alert($('.add-gift').length);
+    //$('.add-gift-div').css('color','red');
+    //$('h1').css('color','red');
+    //$('.add-gift').fadeOut();
+    //	alert('hello');
+    //	window.close();
+
+    //window.close();
+    //	$('add-gift').fadeOut();
+  });
+
 });
 
