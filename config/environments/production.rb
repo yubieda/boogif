407d79ddbc82b4d296cb1bf0eb8e49493a3b4cdf
Alex Mishyn <mishyn@gmail.com>
2013-02-19 09:00:49 +0200

    add js to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ BasicSite::Application.configure do
    config.action_controller.asset_host = APP_PUBLIC_URL
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( add_gift.js add_gift_lib.js popup.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
