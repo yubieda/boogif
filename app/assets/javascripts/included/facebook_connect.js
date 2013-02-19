2cc8af852c42ca7c4c65d27288f0890831cc623e
Alex Mishyn <mishyn@gmail.com>
2013-02-19 11:22:00 +0200

    change fb app id

--- app/assets/javascripts/included/facebook_connect.js
+++ app/assets/javascripts/included/facebook_connect.js
@@ -1,7 +1,7 @@
 $(function(){
   $('body').append('<div id="fb-root"></div>');
   window.fbAsyncInit = function() {
-    FB.init({appId: '387775747964012', status: true, cookie: true, xfbml: true});
+    FB.init({appId: '607574899257942', status: true, cookie: true, xfbml: true});
   };
   $.getScript(document.location.protocol + '//connect.facebook.net/en_US/all.js');
 
