e40375d61be1aaf5f0d0a2381f02a442891a112b
ronald05arias <ronald05arias@gmail.com>
2013-02-24 17:09:46 -0430

    testing enter html

--- app/assets/javascripts/included/facebook_connect.js
+++ app/assets/javascripts/included/facebook_connect.js
@@ -11,10 +11,10 @@ $(function(){
         name: 'BOOGiF - The Right Gift, The Easy Way!',
         link: 'http://warm-crag-9574.herokuapp.com/',
         description: (
-        	'Do you want to find the right gift and always receive what you want?\n'+
-			'Then, BOOGiF is the solution you need.\n'+
-			  '\t*Find the right gift for someone special in a few seconds.\n'+
-			  '\t*And make others always give you the things you love.\n'+
+        	'Do you want to find the right gift and always receive what you want?<br/>'+
+			'Then, BOOGiF is the solution you need.<br/>'+
+			  '*Find the right gift for someone special in a few seconds.<br/>'+
+			  '*And make others always give you the things you love.</br/>'+
 			'Join today at www.boogif.com'
 	   ),
 	   picture: 'http://warm-crag-9574.herokuapp.com/assets/BG128x128.png'
